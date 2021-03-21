dbfd6d5a62aabfe6ba62beaff969d411a9ae4ef3
firstTrip <hdnwls961@gmail.com>
2021-03-22 03:31:48 +0900

    test

---
+++ Assets/Resources/Script/CameraMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    Camera camera;
+    LayerMask layer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Initialized();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Initialized()
+    {
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+}
